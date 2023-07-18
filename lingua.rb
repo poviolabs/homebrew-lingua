c0826d2e6cf8ab128a68696f4fc11f8d2a557532
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-07-18 11:00:50 +0200

    update sha256 and url

--- lingua.rb
+++ lingua.rb
@@ -1,8 +1,8 @@
 class Lingua < Formula
   desc "Unified localization management tool for iOS & Android via Google Sheets"
   homepage "https://github.com/poviolabs/Lingua"
-  url "https://github.com/poviolabs/Lingua/archive/0.2.1.tar.gz"
-  sha256 "8288b4702a4a42494dbdd5aedb01884a340653f7dcfd99bc4da33d441c14d555"
+  url "https://github.com/poviolabs/Lingua/archive/0.2.2.tar.gz"
+  sha256 "8cef62ebbc07253cd2ddd7b7f8ba39e1506cfa8b788945669f6a3e4483f5b825"
 
   depends_on :macos
   depends_on :xcode => ["14.1", :build]
