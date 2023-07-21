3125343ef0192b0cf56708941d1d60e3417d2616
Egzon Arifi <egzon.r.arifi@gmail.com>
2023-07-21 14:20:04 +0200

    universal binary

--- lingua.rb
+++ lingua.rb
@@ -1,17 +1,13 @@
 class Lingua < Formula
   desc "Unified localization management tool for iOS & Android via Google Sheets"
   homepage "https://github.com/poviolabs/Lingua"
-  url "https://github.com/poviolabs/Lingua/archive/0.2.2.tar.gz"
-  sha256 "8cef62ebbc07253cd2ddd7b7f8ba39e1506cfa8b788945669f6a3e4483f5b825"
-
-  depends_on :macos
-  depends_on :xcode => ["14.1", :build]
-
+  url "https://github.com/poviolabs/Lingua/releases/download/0.2.2/Lingua"
+  sha256 "21da16a6e8bc44e25983897590340842ccb6a3723ec1e85509dcb28035901a3b"
+  
   def install
-    system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
-    bin.install '.build/release/Lingua'
+    bin.install "Lingua"
   end
-
+    
   test do
     system "#{bin}/Lingua", "--version"
   end
